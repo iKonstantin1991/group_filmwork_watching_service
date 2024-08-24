9eeec073ea3275995afca26ea644e629fe9c6bd8
Ekaterina <cherednichenko_kate@mail.ru>
2024-08-24 12:47:53 +0300

    feat: add models and core

---
+++ booking-service/src/db/postgres.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+
+from core.config import settings
+
+Base = declarative_base()
+dsn = (f'postgresql+asyncpg://{settings.postgres_user}:{settings.postgres_password}@'
+       f'{settings.postgres_host}:{settings.postgres_port}/{settings.postgres_db}')
+engine = create_async_engine(dsn, echo=settings.echo_in_db, future=True)
+async_session = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
