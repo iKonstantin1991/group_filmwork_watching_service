ab27087135797043419fb207ba19e0b25c5e9cd0
iKonstantin1991 <ikonstantin1991@mail.ru>
2024-09-11 10:03:53 +0300

    fix tests for places

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,66 +1,24 @@
-from dataclasses import dataclass
-from typing import Awaitable, Dict, Iterator
-import asyncio
-from uuid import UUID, uuid4
+from uuid import uuid4
 
-from aiohttp import ClientSession, ClientResponse
-import pytest_asyncio
+import jwt
+import pytest
 
-from tests.utils import _get_token
-
-_BASE_URL = f'http://test'
+from tests.config import settings
+from tests.models import User
 
 
-class Client:
-    def __init__(self, session: ClientSession) -> None:
-        self._session = session
-
-    async def get(
-        self, path: str, params: Dict[str, str] | None = None, headers: Dict[str, str] | None = None
-    ) -> ClientResponse:
-        return await self._session.get(f'{_BASE_URL}/{path}', params=(params or {}), headers=(headers or {}))
-
-    async def post(
-        self, path: str, body: Dict[str, str] | None = None, headers: Dict[str, str] | None = None
-    ) -> ClientResponse:
-        return await self._session.post(f'{_BASE_URL}/{path}', json=(body or {}), headers=(headers or {}))
-
-    async def delete(self, path: str, headers: Dict[str, str] | None = None) -> ClientResponse:
-        return await self._session.delete(f'{_BASE_URL}/{path}', headers=(headers or {}))
+@pytest.fixture(name="user")
+def fixture_user() -> User:
+    return get_random_user()
 
 
-@dataclass(frozen=True)
-class User:
-    __test__ = False
-
-    id: UUID
-    token: str
-
-
-@pytest_asyncio.fixture(scope='session')
-def event_loop() -> Iterator[asyncio.AbstractEventLoop]:
-    loop = asyncio.get_event_loop()
-    yield loop
-    loop.close()
-
-
-@pytest_asyncio.fixture(scope='session', name='aiohttp_session')
-async def fixture_aiohttp_session() -> Iterator[ClientSession]:
-    session = ClientSession()
-    yield session
-    await session.close()
-
-
-@pytest_asyncio.fixture
-async def client(aiohttp_session: ClientSession) -> Awaitable[ClientResponse]:
-    return Client(aiohttp_session)
-
-
-@pytest_asyncio.fixture(name='user')
-async def fixture_user() -> Iterator[User]:
-    pass
-
-
-async def get_random_user() -> User:
+def get_random_user() -> User:
     user_id = uuid4()
-    return User(id=user_id, token=_get_token(user_id))
+    return User(
+        id=user_id,
+        token=jwt.encode(
+            {"user_id": str(user_id), "roles": ["subscriber"]},
+            settings.test_private_key,
+            algorithm=settings.token_algorithm,
+        ),
+    )
---
+++ tests/src/test_places.py
@@ -0,0 +1,83 @@
+import uuid
+from http import HTTPStatus
+
+import httpx
+
+from tests.config import settings
+from tests.conftest import get_random_user
+from tests.models import User
+
+
+def test_get_place_by_place_id_correctly(user: User) -> None:
+    name, address = "some place", "some address"
+    expected_place = {"host": str(user.id), "status": "open", "name": name, "address": address}
+    response = httpx.post(
+        f"{settings.service_url}/api/v1/places/",
+        json={"name": name, "address": address, "city": "some city"},
+        headers=_build_headers(user.token),
+    )
+    assert response.status_code == HTTPStatus.OK
+    _assert_place(response.json(), expected_place)
+    created_place_id = response.json()["id"]
+
+    response = httpx.get(f"{settings.service_url}/api/v1/places/{created_place_id}", headers=_build_headers(user.token))
+    assert response.status_code == HTTPStatus.OK
+    _assert_place(response.json(), expected_place)
+
+
+def test_get_places_by_host_correctly() -> None:
+    user_one, user_two = get_random_user(), get_random_user()
+    name_one, address_one = "some place one", "some address one"
+    name_two, address_two = "some place two", "some address two"
+    expected_place_one = {"host": str(user_one.id), "status": "open", "name": name_one, "address": address_one}
+    expected_place_two = {"host": str(user_two.id), "status": "open", "name": name_two, "address": address_two}
+    response = httpx.post(
+        f"{settings.service_url}/api/v1/places/",
+        json={"name": name_one, "address": address_one, "city": "some city"},
+        headers=_build_headers(user_one.token),
+    )
+    _assert_place(response.json(), expected_place_one)
+
+    created_place = httpx.post(
+        f"{settings.service_url}/api/v1/places/",
+        json={"name": name_two, "address": address_two, "city": "some city"},
+        headers=_build_headers(user_two.token),
+    )
+    _assert_place(created_place.json(), expected_place_two)
+
+    response = httpx.get(
+        f"{settings.service_url}/api/v1/places/?host_id={user_one.id}", headers=_build_headers(user_one.token)
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert len(response.json()) == 1
+    _assert_place(response.json()[0], expected_place_one)
+
+
+def test_close_existing_place_without_watches_correctly(user: User) -> None:
+    name, address = "some place", "some address"
+    expected_place = {"host": str(user.id), "status": "open", "name": name, "address": address}
+    response = httpx.post(
+        f"{settings.service_url}/api/v1/places/",
+        json={"name": name, "address": address, "city": "some city"},
+        headers=_build_headers(user.token),
+    )
+    assert response.status_code == HTTPStatus.OK
+    _assert_place(response.json(), expected_place)
+    created_place_id = response.json()["id"]
+
+    response = httpx.delete(
+        f"{settings.service_url}/api/v1/places/{created_place_id}",
+        headers=_build_headers(user.token),
+    )
+    assert response.status_code == HTTPStatus.OK
+
+
+def _assert_place(actual: dict, expected: dict) -> None:
+    assert "id" in actual
+    assert "created_at" in actual
+    for key, value in expected.items():
+        assert actual[key] == value
+
+
+def _build_headers(token: str) -> dict:
+    return {"X-Request-Id": str(uuid.uuid4()), "Authorization": f"Bearer {token}"}
